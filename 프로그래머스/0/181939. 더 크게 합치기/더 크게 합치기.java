aa643780d769e635def09dd8bfbe00a81642dc1a
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-04-30 21:03:57 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.98 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        String s1 = String.valueOf(a);
+        String s2 = String.valueOf(b);
+       
+        if(Integer.valueOf(s1+s2) > Integer.valueOf(s2+s1)){
+            answer = Integer.valueOf(s1+s2);
+            
+        }
+        else{
+            answer = Integer.valueOf(s2+s1);
+        }
+        return answer;
+    }
+}
