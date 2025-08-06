861721f637d23fc2d45e05f8498ca40ea673c1ad
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-08-06 11:32:20 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,17 +1,11 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        String str = String.valueOf(num);
-        char ch = (char)(k + '0');
         
-        for(int i=0; i<str.length(); i++){
-            if(ch == str.charAt(i)){
-                answer =i+1;
-                break;
-            } 
-            else answer =-1;
-              
-        }
-        return answer;
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+
+        int answer = numStr.indexOf(kStr);
+        return answer < 0 ? -1 : answer + 1 ;
+        
     }
 }
