7e4e065d5b3692c65364b2ca85143bb5e9b15232
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-08-12 17:22:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 289.50 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,21 +3,21 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        int parlen = participant.length;
+        int comlen = completion.length;
         
         Arrays.sort(participant);
         Arrays.sort(completion);
         
         //중간에 다른 사람 있으면 그 사람이 미 완주자
-        for(int i=0; i<completion.length; i++){
+        for(int i=0; i<comlen; i++){
             if(!participant[i].equals(completion[i])){
                 return participant[i];
             }
         }
             
-        //끝에 마지막으로 남는 사람이 미완주자
-        return participant[participant.length - 1];
+        //끝에 마지막으로 남는 사람이 미완주자 -> completion길이는 participant보다 1 작기 때문
+        return participant[parlen - 1];
         
-        
-
     }
 }
