acfc189929c6b209bceaa0152b75d39b3b9ff006
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-08-14 14:49:39 +0900

    [level 1] Title: K번째수, Time: 1.02 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,24 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-            
-        List<Integer> answerList = new ArrayList<>();
-
-        int len = commands.length;
+        int[] answer = new int[commands.length];
         
-        for(int i=0; i<len; i++){
-            int start = commands[i][0];
-            int end = commands[i][1];
-            int idx = commands[i][2];
-                    
-            List<Integer> list = new ArrayList<>();
-            for(int j = start-1; j< end; j++){
-                list.add(array[j]);
-            }
-            Collections.sort(list);
-            answerList.add(list.get(idx-1));
+        for(int i=0; i<commands.length; i++){
+            int temp[] = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
         }
-        return answerList.stream().mapToInt(Integer :: intValue).toArray();
+        return answer;
     }
 }
