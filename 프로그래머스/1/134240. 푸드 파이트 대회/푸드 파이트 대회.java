2bcafeceb0de0e49b1978d77e344f3783a44d27d
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-08-18 15:12:20 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 4.13 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] food) {
+
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<food.length; i++){
+            for(int j=0; j<food[i]/2; j++){
+                sb.append(i);
+            }
+        }
+
+        String answer= sb+ "0";
+        answer += sb.reverse();
+           
+
+        return answer;
+    }
+}
