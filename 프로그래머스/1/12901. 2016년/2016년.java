2a3db98ddac5f42778e4f38771458eaca577f40e
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-08-23 23:56:04 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int a, int b) {
+
+        String day[] = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int d[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+        int answer = 0;
+        for(int i=0;i<a-1; i++){
+            answer+=d[i];
+        }
+        answer +=b-1;
+
+        return day[answer % 7];
+    }
+}
