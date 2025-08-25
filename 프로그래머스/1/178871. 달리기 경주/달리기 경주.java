e2878795565525922688f6eee9593975aaa1e544
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-08-25 21:57:07 +0900

    [level 1] Title: 달리기 경주, Time: 233.74 ms, Memory: 375 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        //바로 앞 선수 추월하면 추월한 선수의 이름 부름
+        //현재 등수 players
+        //해설진이 부른 순서 callings
+        
+        String[] answer = Arrays.copyOf(players, players.length);
+        
+        //hashmap으로 저장해주기
+        Map<String, Integer> map = new HashMap<>(); //현재 등수 map
+                
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
+        
+        for(String str : callings){
+            int idx = map.get(str); //일치하는 선수의 인덱스 구해주고
+            int frontIdx = map.get(str) -1;
+                                
+            String temp = answer[frontIdx];
+            answer[frontIdx] = answer[idx];
+            answer[idx] = temp;
+            
+            map.put(str,frontIdx);
+            map.put(temp, idx);
+        }
+        
+
+        return answer;
+    }
+}
