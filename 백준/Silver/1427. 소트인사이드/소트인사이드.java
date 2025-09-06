2fabb5f869438a18be1b84d3de12facc5aa431cd
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-06 18:25:48 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        char arr[] = N.toCharArray();
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder(new String(arr));
+        System.out.println(sb.reverse());
+
+        br.close();
+
+    }
+}
