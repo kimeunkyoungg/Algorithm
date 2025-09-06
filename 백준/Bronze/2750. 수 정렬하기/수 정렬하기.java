8a181f0ce987ad568d633277f51d70e1de645959
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-06 18:12:42 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 14896 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        //버블 정렬 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i=0; i<N-1; i++){
+            for(int j=0; j<N-1-i; j++){
+                if(arr[j+1]<arr[j]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        for(int i=0; i<N; i++){
+            System.out.println(arr[i]);
+        }
+        br.close();
+
+    }
+}
