648b66be4675c35d077e5796e5e0ce6ff32e21a4
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-08 17:26:47 +0900

    [Silver V] Title: K번째 수, Time: 1924 ms, Memory: 377492 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int answer = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken()); //앞에서부터 k번째 있는 수 구해야함
+
+        int arr[] = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        answer = arr[K-1];
+        
+        System.out.println(answer);
+
+        br.close();
+
+    }
+}
