ec28336dbd81249bcfdb280c3ac6ed83098a6219
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-09 17:04:50 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1004 ms, Memory: 119576 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,80 @@
-import java.util.*;
 import java.io.*;
 
-public class Main{
+//병합 정렬 사용
+public class Main {
 
-    public static void main(String[] args) throws IOException{
-     
+    public static int[] A;
+    public static int[] tmp; //정렬할 때 사용할 배열
+    public static int result;
+
+    public static void main(String[] args) throws IOException {
+
+        int answer = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        A = new int[N+1];
+        tmp = new int[N+1];
 
-        
-
-        for(int i =0; i<n; i++){
-            list.add(Integer.parseInt(br.readLine()));
+        for(int i=1; i<=N; i++){
+            A[i] = Integer.parseInt(br.readLine());
         }
 
-        Collections.sort(list);
+        merge_sort(1, N);
 
-        for(int value : list){
-            sb.append(value).append('\n');
-
+        for(int i=1; i<=N; i++){
+            bw.write(A[i] +"\n");
         }
-        System.out.println(sb);
+
+        bw.flush();
+        br.close();
+
+    }
+
+    public static void merge_sort(int s, int e){
+        if(e-s < 1){
+            return;
+        }
+
+        int m = (s + e) / 2;
+        //재귀 함수 사용
+        merge_sort(s, m);
+        merge_sort(m+1, e);
+
+        for(int i = s; i<=e; i++){
+            tmp[i] = A[i];
+        }
+
+        int k = s;
+        int idx1 = s;
+        int idx2 = m+1;
+
+        while(idx1 <=m && idx2 <=e){ //두 그룹 병합
+            if(tmp[idx1] > tmp[idx2]){
+                A[k] = tmp[idx2];
+                k++;
+                idx2++;
+            }
+            else{
+                A[k] = tmp[idx1];
+                k++;
+                idx1++;
+            }
+        }
+
+        while(idx1 <= m){ //한쪽 그룹이 모두 선택된 후 남아있는 값 정리
+            A[k] = tmp[idx1];
+            k++;
+            idx1++;
+        }
+
+        while (idx2 <= e){
+            A[k] = tmp[idx2];
+            k++;
+            idx2++;
+        }
     }
 }
