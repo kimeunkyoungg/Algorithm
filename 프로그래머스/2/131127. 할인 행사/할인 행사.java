a41ad4d9e867dd093b5361f26a9f314a642a8423
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-27 20:07:54 +0900

    [level 2] Title: 할인 행사, Time: 60.23 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i=0; i<want.length; i++){
+            map.put(want[i], number[i]);
+        }
+        
+
+        for(int i=0; i<=discount.length-10; i++){
+            HashMap<String, Integer> countMap = new HashMap<>();
+        
+            for(int j=i; j<i+10; j++){
+                countMap.put(discount[j], countMap.getOrDefault(discount[j], 0) + 1);
+                
+            }
+            
+            if(countMap.equals(map)){
+                answer ++;
+            }
+            
+        }
+        return answer;
+    }
+}
