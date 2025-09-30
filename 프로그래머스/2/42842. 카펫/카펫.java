3f254ef2b123fa9e77c2d8b159cd42b62cf10ab1
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-30 22:17:51 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        //가로 >= 세로
+        int total = brown + yellow;
+        
+        for(int h=3; h<=total; h++){
+            int w = total/h;
+            
+            if(total % w == 0 && w >= h){
+                if((w-2) * (h-2) == yellow){
+                    return new int[]{w,h};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
