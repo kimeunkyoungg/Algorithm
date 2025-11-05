6cd606b3fd53c2331d01f29c6c2a5ce9fabe4b85
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-11-05 20:39:17 +0900

    [level 3] Title: N으로 표현, Time: 28.41 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        //사칙연산 -> 괄호, + * / -
+        int answer = 0;
+        
+        if(number == 1) return 1;
+        
+        List<Set<Integer>> dp = new ArrayList<>();
+        
+        for(int i=0; i<=8; i++){
+            dp.add(new HashSet<>());
+        }
+        
+        for(int i=1; i<=8; i++){
+            int repeated = Integer.parseInt(String.valueOf(N).repeat(i));
+            dp.get(i).add(repeated);
+            
+            for(int j=1; j<i; j++){
+                for(int a : dp.get(j)){
+                    for(int b : dp.get(i-j)){
+                        dp.get(i).add(a+b);
+                        dp.get(i).add(a-b);
+                        dp.get(i).add(a*b);
+                        
+                        if(b!=0){
+                            dp.get(i).add(a/b);
+                        }
+                    }
+                }
+            }
+            
+            if(dp.get(i).contains(number)) return i;
+        
+        }
+        
+        return -1;
+    }
+}
